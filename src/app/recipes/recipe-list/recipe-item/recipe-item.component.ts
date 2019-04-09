c62c4ed2088c6ae96bf28b14b18e1489357068ff
Evgeny Ovchinnikov <e.ovchinnikov@sysolutions.ru>
2019-04-09 11:42:07 +0700

    Some changes

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -8,7 +8,7 @@ import {Recipe} from '../../recipe.model';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-  @Input() recipeItem: Recipe;
+  @Input() recipe: Recipe;
 
   constructor() { }
 
